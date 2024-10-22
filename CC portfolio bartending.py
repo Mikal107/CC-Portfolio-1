a82de34b3c3d2ea4f4dc58ec22fab0d99ae1a481
Mikal107 <mgolds19@u.rochester.edu>
2024-10-22 02:40:29 -0400

    Changed some formatting, wrote get_recipes()

--- CC portfolio bartending.py
+++ CC portfolio bartending.py
@@ -7,7 +7,7 @@ class Recipe:
         self.name = name
         self.vars = [""]
         self.ings = [ings]
-        write_recipe("Recipes.txt", self)
+        write_recipe(self)
     def __repr__(self):
         return self.name
 # Here, var will be input as an int corresponding to the variation's 
@@ -20,48 +20,61 @@ class Recipe:
             self.vars[0] = original_name
         self.vars.append(name)
         self.ings.append(ings)
-        write_recipe("Recipes.txt", self)
+        write_recipe(self)
 
-def write_recipe(file, recipe):
-    infile = open(file, "r")
+def write_recipe(recipe):
+    infile = open("Recipes.txt", "r")
     lines = infile.readlines()
     infile.close()
     for line in lines:
-        if line.split(";")[0] == recipe.name:
+        if line.split(":")[0] == recipe.name:
             lines.remove(line)
             break
 
-    recipe_string = f"{recipe.name};"
+    recipe_string = f"{recipe.name}:"
     for i in range(len(recipe.vars)):
         recipe_string += recipe.vars[i] + ";"
         ing_names = ",".join(recipe.ings[i].keys())
         ing_amounts = ",".join(recipe.ings[i].values())
         recipe_string += ing_names + ";"
-        recipe_string += ing_amounts + ";"
+        recipe_string += ing_amounts
+        if i != len(recipe.vars) - 1:
+            recipe_string += ":"
     recipe_string += "\n"
     lines.append(recipe_string)
     lines.sort()
 
-    outfile = open(file, "w")
+    outfile = open("Recipes.txt", "w")
     for line in lines:
         outfile.write(line)
     outfile.close()
 
-# This code block pretends recipes are structured as in booze.py
-# Will need to be rewritten once Recipes.txt is reformatted
-# recipes = []
-# infile = open("Recipes.txt", "r")
-# lines = [line.split(",") for line in infile.readlines()]
-# for line in lines:
-#     ing_names, amounts = [], []
-#     for i in range(1, len(line)):
-#         if i % 2 == 1: ing_names.append(line[i])
-#         elif i % 2 == 0: amounts.append(line[i])
-#     ings = {}
-#     for i in range(len(ing_names)):
-#         ings[ing_names[i]] = amounts[i]
-#     recipes.append(Recipe(line[0], ings))
-# print(recipes)
+def get_recipes():
+    recipes = []
+    infile = open("Recipes.txt", "r")
+    lines = infile.readlines()
+    infile.close()
+    for line in lines:
+        line = line.strip("\n")
+        name = line.split(":")[0]
+        orig = line.split(":")[1]
+        vars = line.split(":")[2:]
+
+        orig_name = orig.split(";")[0]
+        ing_names = orig.split(";")[1].split(",")
+        amounts = orig.split(";")[2].split(",")
+        ings = {k:v for (k,v) in zip(ing_names, amounts)}
+        recipe = Recipe(name, ings)
+
+        for var in vars:
+            var_name = var.split(";")[0]
+            ing_names = var.split(";")[1].split(",")
+            amounts = var.split(";")[2].split(",")
+            var_ings = {k:v for (k,v) in zip(ing_names, amounts)}
+            recipe.add_var(var_name, var_ings, orig_name)
+        
+        recipes.append(recipe)
+    return recipes
 
 ##### TESTING AREA #####
 ##### TESTING AREA #####
@@ -73,3 +86,7 @@ blood_and_sand = Recipe("Blood and Sand", bs_ings)
 bsm_ings = {"Scotch":"1.5 oz", "Cherry Heering":"0.5 oz",
            "Sweet Vermouth":"0.5 oz", "Orange Juice":"0.75 oz"}
 blood_and_sand.add_var("Meehan", bsm_ings, "Classic")
+bss_ings = {"Blood":"1.5 oz", "Sand":"1.5 oz"}
+blood_and_sand.add_var("Authentic", bss_ings)
+
+recipes = get_recipes()
